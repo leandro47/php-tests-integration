2ccc4e5c37a1ae8879a05ab0e056fcd30ed073a7
leandro47 <leandrosilva47@live.com>
2021-10-30 18:27:01 -0300

    adicionando novo metodo

--- tests/Integration/Dao/LeilaoDaoTest.php
+++ tests/Integration/Dao/LeilaoDaoTest.php
@@ -46,6 +46,67 @@ class LeilaoDaoTest extends TestCase
         );
     }
 
+    /**
+     * @dataProvider leiloes
+     */
+    public function testBuscaLeiloesNaoFinalizados(array $leiloes)
+    {
+        // arrange
+        $leilaoDao = new LeilaoDao(self::$pdo);
+        foreach ($leiloes as $leilao) {
+            $leilaoDao->salva($leilao);
+        }
+
+        // act
+        $leiloes = $leilaoDao->recuperarNaoFinalizados();
+
+        // assert
+        self::assertCount(1, $leiloes);
+        self::assertContainsOnlyInstancesOf(Leilao::class, $leiloes);
+        self::assertSame(
+            'Variante 0Km',
+            $leiloes[0]->recuperarDescricao()
+        );
+        self::assertFalse($leiloes[0]->estaFinalizado());
+    }
+
+    /**
+     * @dataProvider leiloes
+     */
+    public function testBuscaLeiloesFinalizados(array $leiloes)
+    {
+        // arrange
+        $leilaoDao = new LeilaoDao(self::$pdo);
+        foreach ($leiloes as $leilao) {
+            $leilaoDao->salva($leilao);
+        }
+
+        // act
+        $leiloes = $leilaoDao->recuperarFinalizados();
+
+        // assert
+        self::assertCount(1, $leiloes);
+        self::assertContainsOnlyInstancesOf(Leilao::class, $leiloes);
+        self::assertSame(
+            'Fiat 147 0Km',
+            $leiloes[0]->recuperarDescricao()
+        );
+        self::assertTrue($leiloes[0]->estaFinalizado());
+    }
+
+    public function leiloes()
+    {
+        $naoFinalizado = new Leilao('Variante 0Km');
+        $finalizado = new Leilao('Fiat 147 0Km');
+        $finalizado->finaliza();
+
+        return [
+            [
+                [$naoFinalizado, $finalizado]
+            ]
+        ];
+    }
+
     protected function tearDown(): void
     {
         self::$pdo->rollBack();
